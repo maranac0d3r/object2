90962f178b4fc0d585f7ceddda60a588ca5674a4
maranac0d3r <maranac0d3r@gmail.com>
2019-05-03 14:37:02 +0530

    get method added

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
-module.export =  funciton()
-{
-    console.log("hahaha");
+
+
+function  get(path,object) {
+    path =Object.prototype.toString.call(path)==="[object Array]"?path: path.split(".");
+    var len = path.length;
+    if(!len){
+        return void  0;
+    }
+    var obj = object;
+    var index = -1;
+
+    while(++index < len){
+        if(Object(obj)!==obj){
+            return void 0;
+        }
+        obj = obj[path[index]]
+    }
+    return  obj;
+}
+
+module.export =  {
+    get:get
 }
